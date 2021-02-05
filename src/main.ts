3a47c410196f583086c88d4157e4320e145d33ce
flashscript <rubensaircortez@gmail.com>
2021-02-04 23:07:15 -0400

    build: change get endpoint to env

--- src/main.ts
+++ src/main.ts
@@ -17,14 +17,14 @@ Vue.use(VueApollo)
 
 Vue.config.productionTip = false
 
-const httpLink = setContext(() => ({headers: {authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYxMjQ3MTk3NiwiZXhwIjoxNjE1MDYzOTc2LCJuYmYiOjE2MTI0NzE5NzYsImp0aSI6IkRpR09nd0dET2xRaTNZdGgiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.8jU7PwD1hctJvoYo17knfXE3x4cIe4__XFIs4KH2jtU`}})).concat(createHttpLink({uri: `http://localhost:8000/graphql`}))
+const httpLink = setContext(() => ({headers: {authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYxMjQ3MTk3NiwiZXhwIjoxNjE1MDYzOTc2LCJuYmYiOjE2MTI0NzE5NzYsImp0aSI6IkRpR09nd0dET2xRaTNZdGgiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.8jU7PwD1hctJvoYo17knfXE3x4cIe4__XFIs4KH2jtU`}})).concat(createHttpLink({uri: `${process.env.VUE_APP_API_URL}/graphql`}))
 
 const cache = new InMemoryCache()
 
 const pusherLink = new PusherLink({
   pusher: new Pusher("528a43e52a265f4b5f54", {
     cluster: "us2",
-    authEndpoint: `http://localhost:8000/graphql/subscriptions/auth`,
+    authEndpoint: `${process.env.VUE_APP_API_URL}/graphql/subscriptions/auth`,
     auth: {
       headers: {
         Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYxMjQ3MTk3NiwiZXhwIjoxNjE1MDYzOTc2LCJuYmYiOjE2MTI0NzE5NzYsImp0aSI6IkRpR09nd0dET2xRaTNZdGgiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.8jU7PwD1hctJvoYo17knfXE3x4cIe4__XFIs4KH2jtU",
